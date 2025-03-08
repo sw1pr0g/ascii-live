ae5ad41f10957f9b5e4c0dbe57f640536b6ff4f2
Hugo Müller-Downing <github@hu.md>
2025-03-08 16:05:31 +0100

    Adds frames for Etienne

---
+++ frames/brittany.go
@@ -0,0 +1,51 @@
+package frames
+
+var Brittany = DefaultFrameType(brittanyFrame)
+var brittanyFrame = []string{
+`oooooooooooo////////////////////////////////////////////////////////////////////
+ooooooooooooo////////////////oNo////////////////////////////////////////////////
+ooooooooooooooooooooooooooooohdNooooo/////////////////////////////oooooooooooooo
+ooooooooooooooooooooooooooooohdddhhNhooooooooooooooooooooooooooooooooooooooooooo
+ooooooooooooooooooooooooooooohsNdNhhhhhhoooooooooooooooooooooooooooooooooooooooo
+ooooooooooooooooooooooooooooohsNdNddooohhhNNhooooooooooooooooooooooooooooooooooo
+ooooooooooooooooooooooooooooohsNddhNhNoooNNNhoNhoooooooooooooooooooooooooooooooo
+hhooooooooooooooooooooooooooohsNddodddhhooohoNNhhhoooooooooooooooooooooooooooooo
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdNNoooooohoo/hNohN.NNooooooooooooooooooooooooooooo
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhddN////......///hNhoddhoooooooooooooooohhhhooohhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhddNNNNNNNNNNh/.../ohNddhoooooooooooooooooooooooooo
+hhhhhhhhhhhhhhhhohooooooooooooNdhhhhhhhNNNdNoo//..//oNooooooooooo////o//////////
+//////////////////////////////Ns/..........////ooo/../h/////////////////////////
+//////////////////////////////hdhhhhhhhhoooo/.../ohh/./Nh///////////////////////
+//////////////////////////////odNNNNNNdddddssNo..../ho./ddNo////////////////////
+///////////////////////////////h/.......///////oo/...ho..ohNN///////////////////
+///////////////////////////////No/////..........ohh/./hh/../o///////////////////
+////////////////////oooohhhhNNNddhhhhhhhhhhhooooo//oohhNNNhhhhhhhooooo//////////
+/////oohhNNNdddssssssddddNNNNhhdsNhoooooooooooooo/./hh/oohhhhNNNNNNddddddddddddN
+ddssssddNNNhhooo///////////////hsh///////////////////o////////////////////////o/
+ooo///////////////ooohhhNNNNdddd-sdddddddddddNNhhho/hdddddNNNNNhhhhoooo/////////
+/////oohhNNdddss----------ssssss-ssssssssssssddddNhhdddddssssssssssssssssssssddo
+/hNds----------sssddddssssss----- ----------sdddNhdsssssssddddddssdddddddddssh`,
+`ooooooooooooo//////////////////////////////////////////////ooooooooooooooooooooo
+oooooooooooooooooooooooooooooNNoooooo///////oooo/ooooooooooooooooooooooooooooooo
+oooooooooooooooooooooooooooooNdNhooooooooooooooooooooooooooooooooooooooooooooooo
+oooooooooooooooooooooooooooooddNhhNhoooooooooooooooooooooooooooooooooooooooooooo
+ooooooooooooooooooooooooooooodNdhoNhhhNNhooooooooooooooooooooooooooooooooooooooo
+ooooooooooooooooooooooooooooosddNhNhhhhhhhNhhooooooooooooooooooooooooooooooooooo
+oooooooooooooooooooooooooooohsddNhhoNNh/ohhhoNNNhooooooooooooooooooooooooooooohh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhsdsNodNNoddNhohhNhNh/ooooooooooohhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhsddhohhhohNdhohhh/hNhhhNhhhhhhhhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhddh/./......////////ooohdNhhhhhhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhdddNNhNhhoooo//o/......./ooooohhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhooooooooooooooohddNNNNNddNNNNNNNddNhhhoooo//..//ooooooooooooooooooo
+/////////////////////////////NN...........////ohooooohhhhhhooohhhh//////////////
+/////////////////////////////NNhhhhooooo///..............//oohhhhNdh////////////
+/////////////////////////////hNdddddddddddNNNhhhooo////.........../o////////////
+/////////////////////////////h/..////////ooooohhhhhhhhhhhhhhoooo//...///////////
+/////////////////////////////oo/////////............/////ooohhhhhhhhoh//////////
+////////////////oooohhhhhhhhhhdhoooooooooooo//////////..........//oohh//////////
+ohhNNddddssssssssdddddNNNNNNNNsdhhhhhhhhhhhhhhhhhhhhhhhhhhhhhooo////ohhhooo/////
+ddNNNhhooooo//////////////////sd///////////////////////////////oo///hhNNNddddddN
+/////////////ooohhhhNNNNNNNdNNs-dNNNNhhhhhhoooooooooooo/////////////////////////
+oohNNNddsss--------------sssss-ssssssssssssssssddddddddddddssssddddNNhhhhoooo/..
+----------sssssddsssssssssss-------ssssssssssdddddddddddddddddddsss-----sssddh..`,
+}
